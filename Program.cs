06c39a15acf97783eecd99426e4e82979b1182f3
GzINOD <gzinod31@rambler.ru>
2022-11-14 21:05:59 +0300

    Homework done

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("input number1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 =  number1%2;
+
+if(number2==0)
+{
+    Console.WriteLine("yes");
+}
+else Console.WriteLine("no");
+ 
