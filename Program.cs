7d1d91a902c21344d38c5aa89623183ad0c3d222
GzINOD <gzinod31@rambler.ru>
2022-11-14 21:50:17 +0300

    Homework redaction

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 ﻿Console.WriteLine("input number1");
 int number1 = Convert.ToInt32(Console.ReadLine());
-int number2 =  number1%2;
-
-if(number2==0)
+if(number1%2==0)
 {
     Console.WriteLine("yes");
 }
